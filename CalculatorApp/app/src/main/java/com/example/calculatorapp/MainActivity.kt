87b9be34d9f45d53771c0de8b7ad83c335b3bdb3
hrl-2024 <hrl@bu.edu>
2023-09-17 21:12:41 -0400

    backend skeleton code

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.calculatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var zero: Button
+    private lateinit var one: Button
+    private lateinit var two: Button
+    private lateinit var three: Button
+    private lateinit var four: Button
+    private lateinit var five: Button
+    private lateinit var six: Button
+    private lateinit var seven: Button
+    private lateinit var eight: Button
+    private lateinit var nine: Button
+
+    private var currentNum: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button0.setOnClickListener{
+            inputNum(0)
+        }
+
+        binding.button1.setOnClickListener{
+            inputNum(1)
+        }
+    }
+
+    fun inputNum(num: Int) {
+        if (currentNum.equals("0")) {
+            currentNum = num.toString()
+        } else {
+            currentNum += num.toString()
+        }
+        binding.editTextAnswer.setText(currentNum)
     }
 }
