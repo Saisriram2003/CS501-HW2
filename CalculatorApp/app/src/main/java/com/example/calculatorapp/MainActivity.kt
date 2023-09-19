3ea3d2e43742a155514b06c87290d56eec1ef7df
hrl-2024 <hrl@bu.edu>
2023-09-19 17:38:59 -0400

    Code clean up

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -20,49 +20,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.editTextAnswer.setText("0")
 
-        binding.button0.setOnClickListener{
+        binding.button0.setOnClickListener {
             inputNum(0)
         }
 
-        binding.button1.setOnClickListener{
+        binding.button1.setOnClickListener {
             inputNum(1)
         }
 
-        binding.button2.setOnClickListener{
+        binding.button2.setOnClickListener {
             inputNum(2)
         }
 
-        binding.button3.setOnClickListener{
+        binding.button3.setOnClickListener {
             inputNum(3)
         }
 
-        binding.button4.setOnClickListener{
+        binding.button4.setOnClickListener {
             inputNum(4)
         }
 
-        binding.button5.setOnClickListener{
+        binding.button5.setOnClickListener {
             inputNum(5)
         }
 
-        binding.button6.setOnClickListener{
+        binding.button6.setOnClickListener {
             inputNum(6)
         }
 
-        binding.button7.setOnClickListener{
+        binding.button7.setOnClickListener {
             inputNum(7)
         }
 
-        binding.button8.setOnClickListener{
+        binding.button8.setOnClickListener {
             inputNum(8)
         }
 
-        binding.button9.setOnClickListener{
+        binding.button9.setOnClickListener {
             inputNum(9)
         }
 
         binding.buttonDot.setOnClickListener {
             var currText = binding.editTextAnswer.text.toString()
-            if (currText.lastIndexOf('.') ==  -1) {
+            if (currText.lastIndexOf('.') == -1) {
                 currText += "."
                 binding.editTextAnswer.setText(currText)
             }
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity() {
             operator("-")
         }
 
-        binding.buttonMult.setOnClickListener{
+        binding.buttonMult.setOnClickListener {
             operator("*")
         }
 
@@ -113,115 +113,118 @@ class MainActivity : AppCompatActivity() {
         currText += op
         binding.editTextAnswer.setText(currText)
     }
-}
-fun evaluateExpression(expression: String): Double {
-    val sanitizedExpression = expression.replace(" ", "") // Remove spaces for simplicity
-    val tokens = tokenizeExpression(sanitizedExpression)
-    return evaluate(tokens)
-}
 
-fun tokenizeExpression(expression: String): List<String> {
-    val operators = setOf("+", "-", "*", "/","s")
-    val tokens = mutableListOf<String>()
-    var currentToken = ""
-    var i = 0
-    while (i < expression.length) {
-        var char = expression[i]
-        if (char.toString() in operators) {
-            if (currentToken.isNotEmpty()) {
-                tokens.add(currentToken)
-                currentToken = ""
-            }
-            if(char.toString() == "s"){
-                i += 3
-            }
-            tokens.add(char.toString())
-        } else {
-            currentToken += char
-        }
-        i += 1
+    fun evaluateExpression(expression: String): Double {
+        val sanitizedExpression = expression.replace(" ", "") // Remove spaces for simplicity
+        val tokens = tokenizeExpression(sanitizedExpression)
+        return evaluate(tokens)
     }
 
-    if (currentToken.isNotEmpty()) {
-        tokens.add(currentToken)
-    }
-
-    return tokens
-}
-
-fun evaluate(tokens: List<String>): Double {
-    Log.d(TAG, tokens.toString())
-    val values = mutableListOf<Double>()
-    val operators = mutableListOf<String>()
-
-    for (token in tokens) {
-        Log.d(TAG, operators.toString())
-        when {
-            // If number add it to values
-            token.matches(Regex("[0-9]+")) -> values.add(token.toDouble())
-            // If operator
-            token in setOf("+", "-", "*", "/", "s") -> {
-                while (operators.isNotEmpty() && precedence(operators.last()) >= precedence(token)) {
-                    val operator = operators.removeAt(operators.size - 1)
-                    if (operator == "s"){
-                        val a = values.removeAt(values.size - 1)
-                        val result = a.pow(0.5)
-                        values.add(result)
-                    }
-                    else{
-                        val b = values.removeAt(values.size - 1)
-                        val a = values.removeAt(values.size - 1)
-                        val result = applyOperator(a, b, operator)
-                        values.add(result)
-                    }
-
+    fun tokenizeExpression(expression: String): List<String> {
+        val operators = setOf("+", "-", "*", "/", "s")
+        val tokens = mutableListOf<String>()
+        var currentToken = ""
+        var i = 0
+        while (i < expression.length) {
+            var char = expression[i]
+            if (char.toString() in operators) {
+                if (currentToken.isNotEmpty()) {
+                    tokens.add(currentToken)
+                    currentToken = ""
                 }
-                operators.add(token)
+                if (char.toString() == "s") {
+                    i += 3
+                }
+                tokens.add(char.toString())
+            } else {
+                currentToken += char
             }
-            else -> throw IllegalArgumentException("Invalid token: $token")
-        }
-    }
-    Log.d(TAG, values.toString())
-    Log.d(TAG, operators.toString())
-
-    while (operators.isNotEmpty()) {
-        val operator = operators.removeAt(operators.size - 1)
-        if (operator == "s"){
-            val a = values.removeAt(values.size - 1)
-            val result = a.pow(0.5)
-            values.add(result)
-        }
-        else{
-            val b = values.removeAt(values.size - 1)
-            val a = values.removeAt(values.size - 1)
-            val result = applyOperator(a, b, operator)
-            values.add(result)
+            i += 1
         }
 
+        if (currentToken.isNotEmpty()) {
+            tokens.add(currentToken)
+        }
+
+        return tokens
     }
 
-    if (values.size != 1) {
-        throw IllegalArgumentException("Invalid expression")
+    fun evaluate(tokens: List<String>): Double {
+        Log.d(TAG, tokens.toString())
+        val values = mutableListOf<Double>()
+        val operators = mutableListOf<String>()
+
+        for (token in tokens) {
+            Log.d(TAG, operators.toString())
+            when {
+                // If number add it to values
+                token.matches(Regex("[0-9]+")) -> values.add(token.toDouble())
+                // If operator
+                token in setOf("+", "-", "*", "/", "s") -> {
+                    while (operators.isNotEmpty() && precedence(operators.last()) >= precedence(
+                            token
+                        )
+                    ) {
+                        val operator = operators.removeAt(operators.size - 1)
+                        if (operator == "s") {
+                            val a = values.removeAt(values.size - 1)
+                            val result = a.pow(0.5)
+                            values.add(result)
+                        } else {
+                            val b = values.removeAt(values.size - 1)
+                            val a = values.removeAt(values.size - 1)
+                            val result = applyOperator(a, b, operator)
+                            values.add(result)
+                        }
+
+                    }
+                    operators.add(token)
+                }
+
+                else -> throw IllegalArgumentException("Invalid token: $token")
+            }
+        }
+        Log.d(TAG, values.toString())
+        Log.d(TAG, operators.toString())
+
+        while (operators.isNotEmpty()) {
+            val operator = operators.removeAt(operators.size - 1)
+            if (operator == "s") {
+                val a = values.removeAt(values.size - 1)
+                val result = a.pow(0.5)
+                values.add(result)
+            } else {
+                val b = values.removeAt(values.size - 1)
+                val a = values.removeAt(values.size - 1)
+                val result = applyOperator(a, b, operator)
+                values.add(result)
+            }
+
+        }
+
+        if (values.size != 1) {
+            throw IllegalArgumentException("Invalid expression")
+        }
+
+        return values[0]
     }
 
-    return values[0]
-}
-
-fun precedence(operator: String): Int {
-    return when (operator) {
-        "+", "-" -> 1
-        "*", "/" -> 2
-        "s" -> 3
-        else -> 0
+    fun precedence(operator: String): Int {
+        return when (operator) {
+            "+", "-" -> 1
+            "*", "/" -> 2
+            "s" -> 3
+            else -> 0
+        }
     }
-}
 
-fun applyOperator(a: Double, b: Double, operator: String): Double {
-    return when (operator) {
-        "+" -> a + b
-        "-" -> a - b
-        "*" -> a * b
-        "/" -> a / b
-        else -> throw IllegalArgumentException("Invalid operator: $operator")
+    fun applyOperator(a: Double, b: Double, operator: String): Double {
+        return when (operator) {
+            "+" -> a + b
+            "-" -> a - b
+            "*" -> a * b
+            "/" -> a / b
+            else -> throw IllegalArgumentException("Invalid operator: $operator")
+        }
     }
 }
